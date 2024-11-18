22a6a8b3581326d6e8a15009143de9c71265731a
Tegarbenayacs <113318369+Tegarbenayacs@users.noreply.github.com>
2024-11-18 15:18:57 +0700

    update terakhir hari ini

--- resources/views/profil-siswa.blade.php
+++ resources/views/profil-siswa.blade.php
@@ -1,6 +1,6 @@
 @extends('components.app')
 @section('content')
-    <div class="container bg-[#F1F1F3] p-8 px-20 h-screen">
+    <div class="container bg-[#F7F7F8] p-8 px-20 h-screen">
         <div class="grid grid-cols-2 gap-4 mt-8">
             <div class="text-gray-800 mb-12"><span class="text-5xl font-bold">Nama Siswa</span><small> Pertemuan
                     ke-$</small></div>
